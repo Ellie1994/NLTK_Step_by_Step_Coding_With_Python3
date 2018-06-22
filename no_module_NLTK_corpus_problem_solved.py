a0b0b99711c283b7687f71295bab67b934ff0a7c
Elvira <40444030+Ellie1994@users.noreply.github.com>
2018-06-22 22:51:26 +0200

    Add files via upload

---
+++ no_module_NLTK_corpus_problem_solved.py
@@ -0,0 +1,18 @@
+# When you use import nltk and get an error:
+
+#"No module named NLTK.corpus"
+
+# You could try doing following:
+# Start your python.
+ 
+# $ python
+# >>>import nltk
+# >>>nltk.download('all')
+
+# it might take you awhile to complete the 'all' download. Be patient. :)
+
+# when the download is completed exit python.
+
+# $ pip install --upgrade nltk
+
+# Hope that helped!
