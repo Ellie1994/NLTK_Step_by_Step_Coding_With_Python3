ad1bcadd7986471342bad7f52f65f4f4f42a205c
Elvira <40444030+Ellie1994@users.noreply.github.com>
2018-06-25 21:55:28 +0200

    Add files via upload

---
+++ 6.find_the_sent.py
@@ -0,0 +1,14 @@
+# prints the sentence if you give its number
+
+from nltk.corpus import gutenberg
+
+caesar_sents = gutenberg.sents("shakespeare-caesar.txt")
+print(caesar_sents[157]) # in [] give a number of a sentence you want to find
+
+#######################################
+# print th elongest sentence
+
+longest_sent = max(len(s) for s in caesar_sents)
+
+print(longest_sent)
+# the longest sentence is the sentence Nr. 157.
